fd5bcae2534ec6fb53ea1ffbc60a4c2379385cb3
Matheus Luís <matheus.bernardi@lnls.br>
2024-02-02 08:20:13 -0300

    caput and cainfo routes

--- app/routers/ca.py
+++ app/routers/ca.py
@@ -8,6 +8,14 @@ router = APIRouter(
 )
 
 
+@router.get("/info/{pv_name}")
+async def cainfo(pv_name: str):
+    res = next((pv for pv in pv_list if pv.name == pv_name), None)
+    if res is None:
+        raise HTTPException(status_code=404, detail="PV not found")
+    return res
+
+
 @router.get("/get/{pv_name}")
 async def caget(pv_name: str):
     res = next((pv for pv in pv_list if pv.name == pv_name), None)
@@ -21,4 +29,23 @@ async def caget_many(pv: str):
     res = [v for v in pv_list if v.name in pv]
     if len(res) == 0:
         raise HTTPException(status_code=404, detail="PVs not found")
+    return res
+
+
+@router.put("/put/{pv_name}")
+async def caput(pv_name: str, value: float):
+    res = next((pv for pv in pv_list if pv.name == pv_name), None)
+    if res is None:
+        raise HTTPException(status_code=404, detail="PV not found")
+    res.value = value
+    return res
+
+
+@router.put("/put_many")
+async def caput_many(pv: str, value: float):
+    res = [v for v in pv_list if v.name in pv]
+    if len(res) == 0:
+        raise HTTPException(status_code=404, detail="PVs not found")
+    for r in res:
+        r.value = value
     return res
